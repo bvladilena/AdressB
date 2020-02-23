0d863f3466fec7da00b0f39b601c525ff4f21445
vladilenababina <vladilena_babina_01@icloud.com>
2020-02-24 01:49:05 +0300

    сделана часть

--- src/main/java/Adr.java
+++ src/main/java/Adr.java
@@ -1,2 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Adr {
+
+    class AdrNew {
+        public int numberH;
+        public int numberF;
+        public String numberS;
+
+        AdrNew(String numberS, int numberH, int numberF) {
+            this.numberS = numberS;
+            this.numberH = numberH;
+            this.numberF = numberF;
+        }
+
+
+        public String toString() {
+            return "street" + numberS + "house" + numberH + "flat" + numberF;
+        }
+    }
+
+    class Name {
+        public String name;
+
+        Name(String name) {
+            this.name = name;
+        }
+
+        public String toString() {
+            return "name" + name + " ";
+        }
+    }
 }
+
+
